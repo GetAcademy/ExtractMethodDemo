d9bc3f2803df01144c36d84dd3587b532fad6823
GET Academy <terje@kolderup.net>
2019-08-08 09:28:14 +0200

    Update Program.cs

--- ExtractMethodDemo/Program.cs
+++ ExtractMethodDemo/Program.cs
@@ -8,40 +8,31 @@ namespace ExtractMethodDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static int FindNextDoubleConsonant(int startPos, string text)
         {
-            var consonants = "bcdfghjklmnpqrstvwxz";
-            var text = "Tre politibetjenter rykket onsdag ut til et hus i Key Haven i Sanford i Florida etter å ha fått en nødsamtale fra telefonen til en tenåring, skriver Sanford Police Department på Facebook.";
-
-            char lastCharacter = '#';
+            var lastCharacter = '#';
             for (var i = 0; i < text.Length; i++)
             {
                 var character = text[i];
-                if (consonants.Contains(character))
-                {
-                    if (character == lastCharacter)
-                    {
-                        lastCharacter = '#';
-                        for (var j = i+1; j < text.Length; j++)
-                        {
-                            character = text[j];
-                            if (consonants.Contains(character))
-                            {
-                                if (character == lastCharacter)
-                                {
-                                    var firstCharacterAfterSecondDoubleConsonant = text[j + 1];
-                                    Console.WriteLine("Første tegn etter andre dobbeltkonsonant er: "
-                                        +firstCharacterAfterSecondDoubleConsonant);
-                                    return;
-                                }
-                            }
-                            lastCharacter = character;
-                        }
-                    }
-
-                }
+                if (IsConsonant(character) && character == lastCharacter) return i;
                 lastCharacter = character;
             }
+            return -1;
+        }
+
+        private static bool IsConsonant(char character)
+        {
+            var consonants = "bcdfghjklmnpqrstvwxz";
+            return consonants.Contains(character);
+        }
+
+
+        static void Main(string[] args)
+        {
+            var text = "Tre politibetjenter rykket onsdag ut til et hus i Key Haven i Sanford i Florida etter å ha fått en nødsamtale fra telefonen til en tenåring, skriver Sanford Police Department på Facebook.";
+            var i1 = FindNextDoubleConsonant(0, text);
+            var i2 = FindNextDoubleConsonant(i1, text);
+            Console.WriteLine("Første tegn etter andre dobbeltkonsonant er: " + text[i2 + 1]);
         }
     }
 }
